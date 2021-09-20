9dd51cec728826733a8dfab99dc3716500091492
Karina Blagushko <karina_blagushko@epam.com>
2021-09-20 16:45:01 +0000

    ac-destination-added

--- srv/libs/syncClientLink.js
+++ srv/libs/syncClientLink.js
@@ -4,7 +4,7 @@ module.exports = async function(req) {
 
         const getTask = await executeHttpRequest(
                 { 
-                    destinationName: 'SCM'
+                    destinationName: req.System
                 },
                 {
                     method: 'get',
@@ -20,13 +20,13 @@ module.exports = async function(req) {
             
         const sendIdToClientLink = await executeHttpRequest(
             { 
-                    destinationName: 'SCM'
+                    destinationName: req.System
             },
             {
                 method: 'patch',
                 url: `/sap/c4c/odata/v1/c4codataapi/CorporateAccountCollection('${ObjectId}')`,
                 data: {
-                    'Z_CallPlannerID_KUT': req.ID
+                    'Z_EmergeID_KUT': req.ID
             },
             headers: {
                 'content-type': 'application/json'
